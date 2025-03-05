164a3977375d21e912881960dc9ff221a015a7da
andersmurphy <andersmurphy@gmail.com>
2025-03-05 21:36:02 +0000

    Only parse json if response it application/json

--- src/hyperlith/impl/http.clj
+++ src/hyperlith/impl/http.clj
@@ -1,14 +1,19 @@
 (ns hyperlith.impl.http
   (:require [org.httpkit.client :as http]
             [hyperlith.impl.json :as json]))
+(defn has-json-body? [resp]
+  (and (:body resp)
+    (-> resp :headers :content-type (= "application/json"))))
 
 (defn get! [url request]
   #_:clj-kondo/ignore
-  (cond-> @(http/get url request)
-    :body (update :body json/try-json->edn)))
+  (let [req @(http/get url request)]
+    (cond-> req
+      (has-json-body? req) (update :body json/try-json->edn))))
 
 (defn post! [url request]
   #_:clj-kondo/ignore
-  (cond-> @(http/post url request)
-    :body (update :body json/try-json->edn)))
+  (let [req @(http/post url request)]
+    (cond-> req
+      (has-json-body? req) (update :body json/try-json->edn))))
 
