e76b23e1a121968c327d2fdf81611e4fe8a6f53a
andersmurphy <andersmurphy@gmail.com>
2025-03-05 23:09:31 +0000

    First draft of http-kit sse client

--- src/hyperlith/impl/http.clj
+++ src/hyperlith/impl/http.clj
@@ -1,19 +1,81 @@
 (ns hyperlith.impl.http
   (:require [org.httpkit.client :as http]
-            [hyperlith.impl.json :as json]))
+            [hyperlith.impl.json :as json]
+            [hyperlith.impl.gzip :as gzip]
+            [clojure.string :as str])
+  (:import [org.httpkit DynamicBytes]
+           [org.httpkit.client IFilter]))
+
 (defn has-json-body? [resp]
   (and (:body resp)
     (-> resp :headers :content-type (= "application/json"))))
 
-(defn get! [url request]
-  #_:clj-kondo/ignore
-  (let [req @(http/get url request)]
-    (cond-> req
-      (has-json-body? req) (update :body json/try-json->edn))))
+(defn wrap-json-response [method]
+  (fn [url request]
+    (let [req @(method url request)]
+      (cond-> req
+        (has-json-body? req) (update :body json/try-json->edn)))))
 
-(defn post! [url request]
-  #_:clj-kondo/ignore
-  (let [req @(http/post url request)]
-    (cond-> req
-      (has-json-body? req) (update :body json/try-json->edn))))
+(def get! (wrap-json-response #_:clj-kondo/ignore http/get))
 
+(def post! (wrap-json-response #_:clj-kondo/ignore http/post))
+
+(defn byte-capture-filter
+  "Captures bytes and returns the last capture of the whole buffer."
+  [capture]
+  (reify IFilter
+    (^boolean accept [_this ^DynamicBytes bytes]
+     ;; As DynamicBytes accumulates we only care about the final result
+     ;; before close or timeout.
+     (reset! capture (.bytes bytes))
+     true)
+    (^boolean accept [_this ^java.util.Map _m]
+     true)))
+
+(def parse-sse-xf
+  (comp ;; Probably a nicer way to write this
+    (remove str/blank?)
+    (map (fn [line] (update (str/split line #": " 2) 0 keyword)))
+    (partition-by #(-> % first (= :event)))
+    (partition-all 2)
+    (map (fn [[a b]]
+           (->> (into a b)
+             (reduce (fn [acc [k v]]
+                       (if (= :data k)
+                         (update acc :data conj v)
+                         (assoc acc k v)))
+               {:data []}))))))
+
+(defn wrap-gzipped-sse-response [method]
+  (fn [url request]
+    (let [capture (atom nil)]
+      #_:clj-kondo/ignore
+      @(method
+         url
+         (assoc request
+           ;; We have to do gzip ourselves otherwise the filter will wait
+           ;; for the stream to complete which could be never.
+           :as :none
+           :filter  (byte-capture-filter capture)))
+      (->> @capture gzip/gunzip str/split-lines
+        (into [] parse-sse-xf)))))
+
+(def sse-get!
+  (wrap-gzipped-sse-response #_:clj-kondo/ignore http/get))
+
+(def sse-post!
+  (wrap-gzipped-sse-response #_:clj-kondo/ignore http/post))
+
+(comment
+  ;; TODO: stream gunzip?
+  ;; TODO: return a channel or atom of SSE events 
+
+  (sse-post!
+    "http://localhost:8080/"
+    {:timeout 1
+     :headers
+     {"Cookie"       "__Host-sid=5SNfeDa90PhXl0expOLFGdjtrpY; __Host-csrf=3UsG62ic9wLsg9EVQhGupw"
+      "Content-Type" "application/json"}
+     :body    "{\"csrf\":\"3UsG62ic9wLsg9EVQhGupw\"}"})
+
+  )
