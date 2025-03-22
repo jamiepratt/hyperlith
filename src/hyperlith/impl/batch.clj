b15c929cda194205ed73d0e4830e2dc8bbcd2b2f
andersmurphy <andersmurphy@gmail.com>
2025-03-22 14:30:33 +0000

    Clean up batch function

--- src/hyperlith/impl/batch.clj
+++ src/hyperlith/impl/batch.clj
@@ -7,13 +7,13 @@
   run every X ms when not empty and/or if it reaches it's max size. Function
   must take a vector of items."
   [effect-fn & {:keys [run-every-ms max-size after-run]
-             :or   {run-every-ms 100
-                    max-size     1000}}]
-  (let [<in (a/chan 1000)]
+                :or   {run-every-ms 100
+                       max-size     1000}}]
+  (let [<in (a/chan 1000)] ;; potentially pass this channel in
     (util/thread
       (loop [<t    (a/timeout run-every-ms)
              batch []]
-        (when-some [[v p] (a/alts!! [<t <in] :priority true)]
+        (let [[v p] (a/alts!! [<t <in] :priority true)]
           (cond
             ;; Reset timer
             (and (= p <t) (= (count batch) 0))
@@ -22,14 +22,18 @@
             ;; Run batch
             (or (= p <t)
               (and (= p <in) (>= (count batch) max-size)))
-            (do
-              (effect-fn (apply concat batch))
-              (after-run)
-              (recur (a/timeout run-every-ms) []))
+            (do (effect-fn (apply concat batch))
+                (after-run)
+                (recur (a/timeout run-every-ms) []))
 
             ;; Add to batch
             (= p <in)
-            (recur <t (conj batch v))))))
+            (recur <t (conj batch v))
+
+            ;; if upstream is close run final batch and stop
+            :else
+            (do (effect-fn (apply concat batch))
+                (after-run))))))
     (fn [items]
       (a/>!! <in items))))
 
